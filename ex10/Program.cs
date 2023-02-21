61112e1ec79674e6c6f216bf3cba48bee7498335
Stas <caplan_74@mail.ru>
2023-02-21 20:15:47 +0500

    DZ2

---
+++ ex10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10
+Console.WriteLine( "Введите трехзначное число" );
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a/10%10;
+Console.WriteLine( "Вторая цифра числа" + "" + a + "" + "=" + x );
