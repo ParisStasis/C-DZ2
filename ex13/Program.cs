eb4debe953ad0363e5a239ce07f882f9408a0967
Stas <caplan_74@mail.ru>
2023-02-21 20:18:36 +0500

    DZ2

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 13;
 
-
+int number = new Random().Next(10,999);
+Console.WriteLine(number);
+int x = number/100;
+int y = number%10;
+if (x > 0)
+{
+    Console.WriteLine( y );
+}
+if (x == 0)
+{
+    Console.WriteLine( "третьей цифры нет" );
+}
--- ex15/Program.cs
+++ ex15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15
+Console.WriteLine("Введите номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a==6)
+{
+    Console.WriteLine("Сегодня выходной, у вас похмелье");
+}
+if (a==7)
+{
+    Console.WriteLine("Сегодня выходной, но завтра на работу");
+}
+if (a<6)
+{
+    Console.WriteLine("Сегодня не выходной, иди работай!!!");
+}
